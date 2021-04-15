a1585b3d14ec61a04a6865a03c10f572e9dd9685
merc312 <merc312@gmail.com>
2021-04-15 16:28:39 -0400

    Set up global grid layout

---
+++ components/card/card.component.jsx
@@ -0,0 +1,11 @@
+import styles from './card.module.css'
+
+const Card = () => {
+  return (
+    <div className={styles.card}>
+      <div>Card Content</div>
+    </div>
+  )
+}
+
+export default Card
